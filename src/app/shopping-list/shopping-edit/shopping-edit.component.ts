86843863931d42b9c5959128a51c6054c64890cd
yurysiruts <sirut.2003@gmail.com>
2021-08-02 23:51:01 +0200

    Shopping List Service

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit, EventEmitter, Output, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Ingredient } from '../../shared/ingredient.model';
+
+import { ShoppingListService } from '../shopping-list.service';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -11,9 +13,8 @@ export class ShoppingEditComponent implements OnInit {
     @ViewChild('nameInput', {static: true}) nameInputRef;
     @ViewChild('amountInput', {static: true}) amountInputRef;
 
-   @Output() ingredientAdded = new EventEmitter<Ingredient>()
-
-  constructor() { }
+  // constructor(private shoppingListService: ShoppingListService) { }
+  constructor(private slService: ShoppingListService) { }
 
   ngOnInit(): void {
   }
@@ -22,7 +23,7 @@ export class ShoppingEditComponent implements OnInit {
       const ingrName = this.nameInputRef.nativeElement.value;
       const ingrAmount = this.amountInputRef.nativeElement.value;
       const newIngredient = new Ingredient(ingrName, ingrAmount);
-      this.ingredientAdded.emit(newIngredient);
+      this.slService.addIngredient(newIngredient)
   }
 
 }
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,3 +1,21 @@
+import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+
+
 export class ShoppingListService {
-  
+  ingredientsChanged = new EventEmitter<Ingredient[]>()
+
+  private ingredients: Ingredient[] = [
+    new Ingredient('Carrots', 2),
+    new Ingredient('Onions', 1),
+  ];
+
+  getIngredients() {
+    return this.ingredients.slice();
+  }
+
+  addIngredient(ingredient: Ingredient) {
+    this.ingredients.push(ingredient)
+    this.ingredientsChanged.emit(this.ingredients.slice())
+  }
 }
